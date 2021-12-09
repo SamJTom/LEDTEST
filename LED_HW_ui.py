f8ffe673ef83acb87b60d9e868be39e95c61f200
SamJTom <89538504+SamJTom@users.noreply.github.com>
2021-12-09 16:50:43 -0600

    Add files via upload

--- LED_HW_ui.py
+++ LED_HW_ui.py
@@ -58,7 +58,7 @@ class Ui_MainWindow(object):
         #self.PressIndicator.setCheckable(True)
         #self.PressIndicator.setChecked(False)
         #self.PressIndicator.isCheckable==False
-        self.short(True)
+        self.short()
         GPIO.setmode(GPIO.BCM)
         pin = 18
         
