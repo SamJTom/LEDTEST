d14d17b7c76c213c0f53f6193c4a7128a872ef06
SamJTom <89538504+SamJTom@users.noreply.github.com>
2021-12-09 01:14:01 -0600

    Add files via upload

--- LED_HW_ui.py
+++ LED_HW_ui.py
@@ -74,6 +74,48 @@ class Ui_MainWindow(object):
         GPIO.add_event_detect(pin, GPIO.RISING)
         if GPIO.event_detected(pin):
             print("Hello, is it me you're looking for?")
+        GPIO.cleanup()
+
+
+
+
+
+        import time
+        import RPi.GPIO as GPIO
+
+        # Use the GPIO numbers, *not* the physical pin numbers
+        GPIO.setmode(GPIO.BCM)
+
+        # Use GPIO 17, which is physical pin 11
+        pin = 17
+        GPIO.setup(pin, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+
+        start_time = time.time()
+        elapsed_time = 0.0
+        while elapsed_time < 10:
+
+            if GPIO.input(pin) == GPIO.HIGH:
+                print("It's pressed!!!")
+
+            time.sleep(0.1) 
+            elapsed_time = time.time() - start_time
+
+
+        def my_callback(channel):
+            print("Hello from the other sideeeeee")
+
+        GPIO.add_event_detect(pin, GPIO.RISING)
+        GPIO.add_event_callback(pin, my_callback)
+
+        print("Now, we wait for an event!  Type Ctl+C to quit.")
+        try:
+            while True:
+                if GPIO.event_detected(pin):
+                    print("Hello, is it me you're looking for?")
+                    break 
+        except KeyboardInterrupt:
+            pass
+
         GPIO.cleanup()    
 
 
